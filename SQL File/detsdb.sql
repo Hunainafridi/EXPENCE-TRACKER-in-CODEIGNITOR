4de8a7ed2e1fb6e1c5dd4364e0e034dc73f2568a
hunainafridi <hunainafridi86@gmail.com>
2026-01-19 21:30:14 +0500

    add files

---
+++ SQL File/detsdb.sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 24, 2025 at 01:50 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `detsdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblexpense`
+--
+
+CREATE TABLE `tblexpense` (
+  `ID` int(10) NOT NULL,
+  `UserId` int(10) NOT NULL,
+  `ExpenseDate` date DEFAULT NULL,
+  `ExpenseItem` varchar(200) DEFAULT NULL,
+  `ExpenseCost` varchar(200) DEFAULT NULL,
+  `NoteDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblexpense`
+--
+
+INSERT INTO `tblexpense` (`ID`, `UserId`, `ExpenseDate`, `ExpenseItem`, `ExpenseCost`, `NoteDate`) VALUES
+(1, 1, '2025-07-03', 'Milk', '257', '2025-07-24 11:36:32'),
+(2, 1, '2025-07-03', 'Bread+buuter', '350', '2025-07-24 11:47:44');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbluser`
+--
+
+CREATE TABLE `tbluser` (
+  `ID` int(10) NOT NULL,
+  `FullName` varchar(150) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Password` varchar(200) DEFAULT NULL,
+  `RegDate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tbluser`
+--
+
+INSERT INTO `tbluser` (`ID`, `FullName`, `Email`, `MobileNumber`, `Password`, `RegDate`) VALUES
+(1, 'John', 'johnd12@gmail.com', 1231231230, 'f925916e2754e5e03f75dd58a5733251', '2025-07-24 11:31:48');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tblexpense`
+--
+ALTER TABLE `tblexpense`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tbluser`
+--
+ALTER TABLE `tbluser`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tblexpense`
+--
+ALTER TABLE `tblexpense`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tbluser`
+--
+ALTER TABLE `tbluser`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ detsci/application/views/includes/footer.php
@@ -0,0 +1,3 @@
+<div class="col-sm-12">
+				<p class="back-link">Daily Expense Tracker <a href="">Hunain Afridi</a></p>
+			</div>
